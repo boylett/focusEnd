3879d8624bd04e0e6a73fd944f802d3ab0c403a2
Boylett <boylett@users.noreply.github.com>
2015-09-14 13:12:09 +0100

    Create jquery.focusEnd.js

---
+++ jquery.focusEnd.js
@@ -0,0 +1,20 @@
+jQuery.fn.focusEnd = function()
+{
+	return $(this).each(function()
+	{
+		var input = $(this).focus(),
+			type = input.is('input, textarea') ? 'val' : 'html';
+		
+		if(this.setSelectionRange && typeof this.setSelectionRange == 'function')
+		{
+			var sel = input[type]().length * 2;
+			this.setSelectionRange(sel, sel);
+		}
+		else
+		{
+			input[type](input[type]());
+		}
+		
+		input[0].scrollTop = input[0].scrollHeight;
+	});
+};
