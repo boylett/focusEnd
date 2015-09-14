00df1ec47e1561cd878568eab65f44b300a7dd90
Boylett <boylett@users.noreply.github.com>
2015-09-14 13:31:55 +0100

    Update jquery.focusEnd.js

--- jquery.focusEnd.js
+++ jquery.focusEnd.js
@@ -2,19 +2,7 @@ jQuery.fn.focusEnd = function()
 {
 	return this.each(function()
 	{
-		var input = $(this).focus(),
-			type = input.is('input, textarea') ? 'val' : 'html';
-		
-		if(this.setSelectionRange && typeof this.setSelectionRange == 'function')
-		{
-			var sel = input[type]().length * 2;
-			this.setSelectionRange(sel, sel);
-		}
-		else
-		{
-			input[type](input[type]());
-		}
-		
-		input[0].scrollTop = input[0].scrollHeight;
+		var type = $(this).focus().is('input, textarea') ? 'value' : 'innerHTML';
+		this.selectionStart = this.selectionEnd = this[type].length;
 	});
 };
